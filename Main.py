cab203e6f411b61be30ee3bcbbcc3701d4af6cf7
Pablo Lino <118943316+PabloLino@users.noreply.github.com>
2024-08-31 15:35:34 -0300

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,12 +1,12 @@
 class Main:
     pass
 
-print("Iniciando Projeto!!!")
+print("\nIniciando Consulta...")
 
 from Cliente import Cliente
 from Conta  import Conta
 
 c1 = Cliente ("Pedro Pedroso", "077.202.869-99")
-conta1 = Conta (c1.nome, '0101', '6500')
+conta1 = Conta (c1._nome, '0101', -100)
 
-print("Titular da conta:",conta1.titular+"\nCPF:",c1.cpf, "\nconta número:", conta1.conta+"\nSaldo dinsponível:", conta1.saldo,"Reais")
+print("\nTitular da conta:",conta1.titular+"\nCPF:",c1._cpf, "\nconta número:", conta1.conta+"\nSaldo dinsponível:", conta1.saldo,"Reais")

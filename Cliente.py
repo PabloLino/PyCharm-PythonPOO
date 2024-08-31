450b79ee14f881c13195a1017d511ce48c668901
Pablo Lino <118943316+PabloLino@users.noreply.github.com>
2024-08-31 15:36:40 -0300

    Update Cliente.py

--- Cliente.py
+++ Cliente.py
@@ -1,4 +1,16 @@
 class Cliente:
-    def __init__ (self, nome, cpf):
-        self.nome = nome
-        self.cpf = cpf
+    def __init__(self, nome, cpf):
+        self._nome = nome
+        self._cpf = cpf
+
+    #Encapasulamento, vou adicionar um "_" em nome e cpf, tornando-os privados.
+    #Ainda podem ser acessados, mas não é o ideal tendo em vista a Convenção em python.
+    #Agora podemos observar a restrição na classe main.
+
+    #Metodo get
+    def get_nome(self):
+        return self._nome
+
+    #Metodo set
+    def set_nome(self, nome):
+        self._nome = nome

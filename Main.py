94411764cf3b4db3436b0576864da0832a015f32
Pablo Lino <118943316+PabloLino@users.noreply.github.com>
2024-09-02 01:35:15 -0300

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,12 +1,30 @@
 class Main:
     pass
 
-print("\nIniciando Consulta...")
+print("\nDigite seus dados")
 
 from Cliente import Cliente
 from Conta  import Conta
 
-c1 = Cliente ("Pedro Pedroso", "077.202.869-99")
-conta1 = Conta (c1._nome, '0101', -100)
+nome_cliente = input("Por favor, insira o nome do cliente: ")
+cpf_cliente = input("Por favor, insira o CPF do cliente: ")
+print("\nIniciando consulta...")
 
-print("\nTitular da conta:",conta1.titular+"\nCPF:",c1._cpf, "\nconta número:", conta1.conta+"\nSaldo dinsponível:", conta1.saldo,"Reais")
+c1 = Cliente(nome_cliente, cpf_cliente)
+##IMPORTANTE: definir aqui o saldo da conta ; )
+conta1 = Conta(c1._nome, '0001', 2500)
+
+print("Titular da conta:",conta1.titular+"\nCPF:",c1._cpf, "\nconta número:", conta1.conta+"\nSaldo dinsponível:",
+conta1.saldo,"Reais")
+
+# Realizando depósito
+valor_deposito = float(input("\nInsira o valor para depósito: "))
+conta1.deposita(valor_deposito)
+print(f"Você depositou R$ {valor_deposito:.2f}. \nSaldo atualizado: R$ {conta1.get_saldo():.2f}")
+
+# Solicitando valor de saque
+valor_saque = float(input("\nInsira o valor para saque: "))
+conta1.saque(valor_saque)
+
+#Verifica extrato
+conta1.extrato()
